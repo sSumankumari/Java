436b5b1dc99b8715d0059273b1d631923fac2108
Suman Kumari <123852425+sSumankumari@users.noreply.github.com>
2024-02-07 11:14:22 +0530

    Practice: Rock/Paper/Scissor game

--- RockPaperScissorGame.java
+++ RockPaperScissorGame.java
@@ -8,31 +8,7 @@ public class RockPaperScissorGame {
         // 2 for Scissor
 
         Scanner sc_input = new Scanner(System.in);
-        System.out.println("Welcome to Play!");
-//        System.out.println("Enter 0 for Rock, 1 for Paper, 2 for Scissor: ");
-//        int userInput = sc_input.nextInt();
-//
-//        Random random = new Random();
-//        int computerInput = random.nextInt(3);
-//
-//        // System.out.println("Computer choice: "+ computerInput);
-//        if(computerInput==0){
-//            System.out.println("Computer choice: Rock");
-//        }
-//        else if(computerInput==1){
-//            System.out.println("Computer choice: Paper");
-//        }
-//        else if(computerInput==2){
-//            System.out.println("Computer choice: Scissors");
-//        }
-//
-//        if (userInput == computerInput) {
-//            System.out.println("Draw");
-//        } else if ((userInput == 0 && computerInput == 2) || (userInput == 1 && computerInput == 0) || (userInput == 2 && computerInput == 1)) {
-//            System.out.println("You Win!");
-//        } else {
-//            System.out.println("Computer Win!");
-//        }
+        System.out.println("Welcome to Rock / Paper / Scissor game!");
 
         System.out.println("Want to play the game: (1 for yes & 0 for no)");
         int play = sc_input.nextInt();
