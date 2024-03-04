9acb74b2fa2374a333e6a5fff021af462a5c203c
Suman Kumari <123852425+sSumankumari@users.noreply.github.com>
2024-03-04 10:59:59 +0530

    Introducing Packages

---
+++ MyPackage.java
@@ -0,0 +1,7 @@
+// package mypackage1;
+
+public class MyPackage {
+    public static void main(String[] args) {
+        System.out.println("This is the main method of the class 'MyPackage'!");
+    }
+}
