a8c3550899b71e77a7d141cb085aa1f6b3340b33
Suman Kumari <123852425+sSumankumari@users.noreply.github.com>
2024-03-08 16:45:14 +0530

    Creating Packages

--- MyPackage.java
+++ MyPackage.java
@@ -1,7 +1,21 @@
-// package mypackage1;
+package mypackage1;
+
+//import java.util.Scanner;
+import java.util.*;
 
 public class MyPackage {
     public static void main(String[] args) {
         System.out.println("This is the main method of the class 'MyPackage'!");
+        System.out.println("Importing Scanner!");
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        java.util.Scanner sc1 = new java.util.Scanner(System.in);
+        int b = sc1.nextInt();
+
+        System.out.println("Taking input as "+ a);
+        System.out.println("Taking input as "+ b);
+
     }
 }
