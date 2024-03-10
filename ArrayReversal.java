e024853c2686cd61ff641e372e14ccaa44ba0757
Suman Kumari <123852425+sSumankumari@users.noreply.github.com>
2024-03-10 23:32:38 +0530

    main

---
+++ ArrayReversal.java
@@ -0,0 +1,31 @@
+public class ArrayReversal {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        System.out.println("Original Array: ");
+        printArray(arr);
+
+        reverseArray(arr);
+
+        System.out.println("Reversed Array: ");
+        printArray(arr);
+    }
+    public static void printArray(int[] arr) {
+        for (int num: arr){
+            System.out.print(num + "\t");
+        }
+        System.out.print("\n");
+    }
+    public static void reverseArray(int[] arr) {
+        int start=0;
+        int end= arr.length-1;
+
+        while (start<end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+}
