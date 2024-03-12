c35793428b32309cb0652cfa7523528c8fb9c8b0
Suman Kumari <123852425+sSumankumari@users.noreply.github.com>
2024-03-12 20:58:08 +0530

    Access modifiers

--- AccessModifiers2.java
+++ AccessModifiers2.java
@@ -3,3 +3,13 @@ public class AccessModifiers2 {
 
     }
 }
+
+/*
+Access Modifier	    within class	  within package	  outside package by subclass only	   outside package
+
+public	                  Y	                Y	                         Y	                        Y
+protected	              Y	                Y	                         Y	                        N
+Default	                  Y	                Y                            N                          N
+private	                  Y	                N	                         N	                        N
+
+ */
