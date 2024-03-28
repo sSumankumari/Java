2cfa98eb05dba976cf8a1d364d0ffa78da5ada53
Suman Kumari <123852425+sSumankumari@users.noreply.github.com>
2024-03-28 22:01:07 +0530

    Thread using Runnable Interface

---
+++ RunnableInterface.java
@@ -0,0 +1,30 @@
+class MyThreadRunnable1 implements Runnable {
+    public void run() {
+        while (true) {
+            System.out.println("This is thread class 1 (gun1 with bullet1)");
+        }
+    }
+}
+
+class MyThreadRunnable2 implements Runnable {
+    public void run() {
+        while (true) {
+            System.out.println("This is thread class 2 (gun2 with bullet2)");
+        }
+    }
+}
+
+public class RunnableInterface {
+    public static void main(String[] args) {
+        System.out.println("Creating Thread by implementing Runnable Interface!");
+
+        MyThreadRunnable1 bullet1 = new MyThreadRunnable1();
+        Thread gun1 = new Thread(bullet1);
+
+        MyThreadRunnable2 bullet2 = new MyThreadRunnable2();
+        Thread gun2 = new Thread(bullet2);
+
+        gun1.start();
+        gun2.start();
+    }
+}
