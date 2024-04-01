f3de25459253c28cadd7236984bcb4e38862be5a
Suman Kumari <123852425+sSumankumari@users.noreply.github.com>
2024-04-01 23:31:44 +0530

    Creating Constructors from Thread

--- Thread_Constructors.java
+++ Thread_Constructors.java
@@ -1,5 +1,29 @@
+class MyThread extends Thread{
+    public MyThread(String name){
+        super(name);
+    }
+    int var=0;
+    public void run(){
+        while (var<6){
+            System.out.println("This is a new thread");
+            var++;
+        }
+    }
+}
+
 public class Thread_Constructors {
     public static void main(String[] args) {
+        MyThread th1 = new MyThread("Sundar Pichai");
+        MyThread th2 = new MyThread("Elon Musk");
 
+        th1.start();
+
+        System.out.println("The id of the thread th is " + th1.getId());
+        System.out.println("The name of the thread th is " + th1.getName());
+
+        th2.start();
+
+        System.out.println("The id of the thread th is "+ th2.getId());
+        System.out.println("The name of the thread th is "+ th2.getName());
     }
 }
